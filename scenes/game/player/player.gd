8543df74209713391588edc0e86feeeeec235428
polyarik <polyarik@pm.me>
2023-03-01 02:09:56 +0700

    ✨ add player walking animation

--- scenes/game/player/player.gd
+++ scenes/game/player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 @export var speed := 64.0
+@onready var animation_tree := $PlayerAnimationTree
+@onready var animation_state_machine = animation_tree.get("parameters/playback")
 
 
 func _physics_process(_delta: float) -> void:
@@ -13,3 +15,12 @@ func _physics_process(_delta: float) -> void:
 	velocity.y = (moveY * speed) if moveY else move_toward(velocity.y, 0, speed)
 
 	move_and_slide()
+	pick_animation_state()
+
+func pick_animation_state() -> void:
+	if (velocity != Vector2.ZERO):
+		animation_state_machine.travel("walk")
+	else:
+		animation_state_machine.travel("idle")
+
+	# TODO - handle attack animation
