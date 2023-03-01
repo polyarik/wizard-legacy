976a06c4f2607d5ac6661a3c4f18650b15238f79
polyarik <polyarik@pm.me>
2023-03-01 23:39:22 +0700

    ✨ implement spell lifetime functionality

--- scenes/game/particles/magic_missile/magic_missile.gd
+++ scenes/game/particles/magic_missile/magic_missile.gd
@@ -4,14 +4,27 @@ extends Area2D
 
 
 var spawned_from: Node = null
+var lifetime := 2.0 # seconds
 var speed := 1.2
 var direction := Vector2.ZERO
 var damage := 5.0
 
 
 func _ready():
+	hanlde_lifetime()
+
 	# TODO - detect the nearest enemy and set direction
 	direction = Vector2(-1, -1).normalized() # TEMP
+
+func hanlde_lifetime() -> void:
+	var lifetime_timer := Timer.new()
+	add_child(lifetime_timer)
+
+	lifetime_timer.wait_time = lifetime
+	lifetime_timer.one_shot = true
+	lifetime_timer.timeout.connect(func(): queue_free())
+
+	lifetime_timer.start()
 
 func _physics_process(_delta: float) -> void:
 	position += direction * speed
